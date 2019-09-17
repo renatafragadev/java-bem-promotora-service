9b1bd0e937111e309b4ecbe2f7ad5ef3b65ea378
Renata Fraga <renatysf@gmail.com>
2019-09-17 00:51:12 -0300

    Initial commit :tada:

---
+++ src/main/java/com/unisinos/engsoftware/bempromotoraprogram/BemPromotoraGraspApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.engsoftware.bempromotoraprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemPromotoraGraspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemPromotoraGraspApplication.class, args);
+	}
+
+}
